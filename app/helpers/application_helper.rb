e30d3cecd220aaad0b420c5517af3160a9608156
Nguyen tuan <dinhtuan1991vt2013@gmail.com>
2015-05-12 18:02:27 +0700

    Render menu item helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  include ActionView::Helpers::TagHelper
+
+  def render_menu_item(path, text)
+    class_name = current_page?(path) ? 'active' : ''
+    content_tag(:li, class: class_name) do
+      link_to text, path
+    end
+  end
 end
